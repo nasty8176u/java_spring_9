07070f59d74eae46978765c74c7a62cc29e62e82
nasty8176u <110005925+nasty8176u@users.noreply.github.com>
2024-02-21 21:38:43 +0900

    Add files via upload

---
+++ DiscoveryMicroService/src/main/java/ru/fsv67/DiscoveryMicroService.java
@@ -0,0 +1,13 @@
+package ru.fsv67;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryMicroService {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryMicroService.class, args);
+    }
+}
